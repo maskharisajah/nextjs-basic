17a8a5db4507a45af247bf5fd50b19c275e7534b
maskharisajah <maskharisajah@gmail.com>
2025-09-18 09:39:12 +0700

    route handler: create endpoint api

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,14 @@
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  return Response.json({
+    message: "User GET Response",
+    user: {
+      id,
+      name: "John Doe",
+    },
+  });
+}
---
+++ app/api/user/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  return Response.json({
+    message: "User GET Response",
+    query,
+  });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  console.log(body);
+  return Response.json({
+    message: "User POST Response",
+  });
+}
+
+export async function PUT() {
+  return Response.json({
+    message: "User PUT Response",
+  });
+}
